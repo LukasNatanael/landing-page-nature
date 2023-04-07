06ee77d6a9f300a6da674c168bfaa1ee260b4529
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2023-04-07 11:56:10 -0300

    mobile semi complete

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ let body = document.querySelector('body')
 let header = document.querySelector('header')
 let input = document.querySelectorAll('.input-personalizado')
 let msg = document.querySelector('#msg')
-let linha = document.querySelector('hr')
+let btnLogin = document.querySelector('#btn-login')
+let borda = document.querySelector('.borda')
 
 logo.addEventListener('click', function() {
     if (body.classList.value == 'light') {
@@ -14,15 +15,17 @@ logo.addEventListener('click', function() {
         body.classList.add('dark')
         header.classList.add('dark-header')
         msg.style.border = '1px solid #F1FAEE'
+        msg.style.background = '#2323237e'
+        msg.style.color = '#F1FAEE'
+        borda.style.borderColor = '#F1FAEE'
+        
+        btnLogin.style.color = '#F1FAEE'
 
-        input[0].style.borderBottom = '1px solid #F1FAEE'
-        input[0].style.color = '#F1FAEE'
-        input[1].style.borderBottom = '1px solid #F1FAEE'
-        input[1].style.color = '#F1FAEE'
-        input[2].style.borderBottom = '1px solid #F1FAEE'
-        input[1].style.color = '#F1FAEE'
-
-        linha.style.color = '#F1FAEE'
+        input.forEach(function (item, indice) {
+            input[indice].style.borderBottom = '1px solid #F1FAEE'
+            input[indice].style.color = '#F1FAEE'
+        })
+        
     }
     else {
         body.classList.remove('dark')
@@ -31,15 +34,15 @@ logo.addEventListener('click', function() {
         body.classList.add('light')
         header.classList.add('light-header')
         msg.style.border = '1px solid #344E41'
+        msg.style.background = '#dad7cd7e'
+        msg.style.color = '#344E41'
+        borda.style.borderColor = '#344E41'
         
-        input[0].style.borderBottom = '1px solid #344E41'
-        input[0].style.color = '#344E41'
-        input[1].style.borderBottom = '1px solid #344E41'
-        input[1].style.color = '#344E41'
-        input[2].style.borderBottom = '1px solid #344E41'
-        input[2].style.color = '#344E41'
+        btnLogin.style.color = '#344E41'
 
-        linha.style.color = '#344E41'
-
+        input.forEach(function (item, indice) {
+            input[indice].style.borderBottom = '1px solid #344E41'
+            input[indice].style.color = '#344E41'
+        })
     }
 })
